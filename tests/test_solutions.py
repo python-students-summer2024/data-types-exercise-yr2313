f207d3ba95f282d4f024a5519469841b95f7ac61
Foo Barstein <amos@cs.nyu.edu>
2020-05-31 00:15:31 -0400

    improved mock input

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -16,28 +16,31 @@ class Tests:
         assert captured.out.lower().strip() == "profit: $2,300.00"
 
     def test_calculate_quotient_and_remainder(self, capsys, monkeypatch):
-        monkeypatch.setattr("builtins.input", lambda x: "5")
+        input_values = ["5", "2"] # the mock values we will use for input
+        monkeypatch.setattr("builtins.input", lambda x: input_values.pop(0))
         calculate_quotient_and_remainder()
         captured = capsys.readouterr() # capture print output
-        expected = "5 goes into 5 a total of 1 times with a remainder of 0".lower().strip()
+        expected = "2 goes into 5 a total of 2 times with a remainder of 1".lower().strip()
         assert captured.out.lower().strip() == expected
 
     def test_calculate_miles_per_gallon(self, capsys, monkeypatch):
-        monkeypatch.setattr("builtins.input", lambda x: "50")
+        input_values = ["50", "2"] # the mock values we will use for input
+        monkeypatch.setattr("builtins.input", lambda x: input_values.pop(0))
         calculate_miles_per_gallon()
         captured = capsys.readouterr() # capture print output
-        expected = "Miles per gallon: 1.0".lower().strip()
+        expected = "Miles per gallon: 25.0".lower().strip()
         assert captured.out.lower().strip() == expected
 
     def test_align_text(self, capsys, monkeypatch):
-        monkeypatch.setattr("builtins.input", lambda x: "50")
+        input_values = ["1", "200", "50"] # the mock values we will use for input
+        monkeypatch.setattr("builtins.input", lambda x: input_values.pop(0))
         align_text()
         captured = capsys.readouterr() # capture print output
         expected = '''
 Here are your prices!
 
-Price #1: $   50.00
-Price #2: $   50.00
+Price #1: $    1.00
+Price #2: $  200.00
 Price #3: $   50.00
         '''.lower().strip()
         assert captured.out.lower().strip() == expected
